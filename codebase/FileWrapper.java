9cf6a8926bfb37107ce55015d5e908434017a6e1
Adi <moritz.adelsberger@gmail.com>
2025-01-16 00:04:18 +0100

    Added some Classes but got tired

---
+++ codebase/FileWrapper.java
@@ -0,0 +1,2 @@
+public class FileWrapper {
+}
---
+++ codebase/Machine.java
@@ -0,0 +1,7 @@
+import java.io.File;
+
+public interface Machines {
+    void writeFile(File file);
+    void removeFile(File file);
+    void existst(File file);
+}
